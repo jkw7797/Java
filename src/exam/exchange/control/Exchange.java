938f91e823972ddc3a780d4bab0d3153e0a8b810
admin <admin@DESKTOP-GK5AJHR>
2021-05-21 12:58:11 +0900

    환전기 구현

---
+++ src/exam/exchange/control/Exchange.java
@@ -0,0 +1,32 @@
+package exam.exchange.control;
+
+import java.util.Scanner;
+
+public class Exchange {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		boolean a = true;
+		while (a) {
+			System.out.println(">돈 넣으세요:\t(0을 입력하면 종료)");
+			int money = scan.nextInt();
+			switch (money) {
+			case 0:
+				a=false;
+				break;
+			default:
+				int ex_money;
+				String unit = "원";
+				if (money < 1200) {
+					ex_money = money * 1200;
+					unit = "원";
+				} else {
+					ex_money = (int) ((double) money / 1200);
+					unit = "달러";
+				}
+				System.out.printf("환전: %d%s\n",ex_money,unit);
+				break;
+			}
+		}
+
+	}
+}
